69fa02f400aa939e21bf28ed10c0c6be4767d3c1
Yuval Bar <yuval4597@gmail.com>
2020-11-24 10:16:07 -0800

    Updated task.cpp to include skeleton fo checkCompletion() and toDo()

--- src/task.cpp
+++ src/task.cpp
@@ -1,1 +1,12 @@
 #include "../header/task.h"
+
+bool Task::checkCompletion()
+{
+    // TODO: Return value based on completionState
+    return true;    // Place holder
+}
+
+void Task::toDo()
+{
+    // TODO: PrintToDo based on completionState
+}
