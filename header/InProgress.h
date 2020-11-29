baca4289b445a64596481b1ea1cdc9ef598d5298
Shawn Long <slong024@ucr.edu>
2020-11-29 14:48:51 -0800

    Wrote headers for concrete State Classes

---
+++ header/InProgress.h
@@ -0,0 +1,11 @@
+#ifndef __INPROGRESS_H__
+#define __INPROGRESS_H__
+
+#include "CompletionState.h"
+
+class InProgress : CompletionState {
+    virtual bool CheckCompletion() override;
+    virtual printToDo() override;
+}
+
+#endif // __INPROGRESS_H__
