a1b2518e38a1fbf750d02998ad0bd783cf5bab5f
Yuval Bar <yuval4597@gmail.com>
2020-12-03 22:42:16 -0800

    Added comments for clarity

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Disable editing of table
     ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
 
