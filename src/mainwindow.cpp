96de27ab7929a5ad95d1121ef20de116a2ce4936
Yuval Bar <yuval4597@gmail.com>
2020-12-03 22:41:15 -0800

    Added basic implementation for addButton

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,3 +13,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// TODO: Create window prompting user to input name, description and due date
+void MainWindow::on_addButton_clicked()
+{
+    // As an example, simply adds rows for now
+    QTableWidget* table = ui->tableWidget;
+    table->setRowCount(table->rowCount() + 1);
+
+    if(table->columnCount() == 0)
+    {
+        table->setColumnCount(1);
+    }
+
+    // Setting name
+    table->setItem(table->rowCount() - 1, 0, new QTableWidgetItem("Task name"));
+
+    // Setting description
+    table->setItem(table->rowCount() - 1, 1, new QTableWidgetItem("Description"));
+
+    // Setting due date
+    table->setItem(table->rowCount() - 1, 2, new QTableWidgetItem("Due date"));
+}
