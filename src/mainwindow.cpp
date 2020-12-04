723d82df45cb5a8278501a85fde3bd27dd224bd8
Yuval Bar <yuval4597@gmail.com>
2020-12-03 23:07:53 -0800

    Modified addButton to add rows after active row instead of end of list

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -23,21 +23,20 @@ void MainWindow::on_addButton_clicked()
 {
     // As an example, simply adds rows for now
     QTableWidget* table = ui->tableWidget;
-    table->setRowCount(table->rowCount() + 1);
 
-    if(table->columnCount() == 0)
-    {
-        table->setColumnCount(1);
-    }
+    // Add row after activeRow, if no row is active add first row
+    int row = activeRow > -1 ? activeRow + 1 : 0;
+
+    table->insertRow(row);
 
     // Setting name
-    table->setItem(table->rowCount() - 1, 0, new QTableWidgetItem("Task name"));
+    table->setItem(row, 0, new QTableWidgetItem("Task name"));
 
     // Setting description
-    table->setItem(table->rowCount() - 1, 1, new QTableWidgetItem("Description"));
+    table->setItem(row, 1, new QTableWidgetItem("Description"));
 
     // Setting due date
-    table->setItem(table->rowCount() - 1, 2, new QTableWidgetItem("Due date"));
+    table->setItem(row, 2, new QTableWidgetItem("Due date"));
 }
 
 void MainWindow::on_tableWidget_cellClicked(int row, int column)
